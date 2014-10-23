688a26ba083253e97345e22fe39973ca4fe85542
Fabien Salvi <fabien.salvi@epfl.ch>
2014-10-23 14:41:05 +0200

    Add php version of typesafe-feed scripts

---
+++ resources/php/typesafe-feed-events.php
@@ -0,0 +1,5 @@
+<?php
+	header('Content-type: application/javascript');
+	$geturl = "http://www.typesafe.com/company/events.json";
+        include "$geturl";
+?>
---
+++ resources/php/typesafe-feed-trainings.php
@@ -0,0 +1,5 @@
+<?php
+	header('Content-type: application/javascript');
+	$geturl = "http://www.typesafe.com/how/training.json";
+        include "$geturl";
+?>
