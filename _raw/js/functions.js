264e2193a962d866605c055764193cb6ee494dc2
israelperezglez <israel.perez.glez@gmail.com>
2017-01-19 12:25:10 +0100

    layout scala nutshell

--- _raw/js/functions.js
+++ _raw/js/functions.js
@@ -10,3 +10,34 @@ $(document).ready(function () {
 $('.open-scala-expanded').click(function(){
     $('.scala-item-expanded').slideToggle('slow');
 });
+
+// Tweet feed in frontpage
+$('#tweet-feed').tweetMachine('', {
+    backendScript: '/resources/php/getFromTwitter.php',
+    endpoint: 'statuses/user_timeline',
+    user_name: 'scala_lang',
+    include_retweets: true,
+    exclude_replies: false,
+    limit: 6,
+    pageLimit: 3,
+    autoRefresh: false,
+    animateIn: false,
+    tweetFormat: `
+    <div class="item-tweet">
+            <img src="" class="avatar" alt="">
+            <div class="tweet-text">
+              <div class="header-tweet">
+                <ul>
+                  <li class="user"><a href="" class="userLink"></a></li>
+                  <li class="username"><a href="" class="usernameLink"></a></li>
+                </ul>
+                <span class="date"></span>
+              </div>
+              <div class="main-tweet"></div>
+            </div>
+          </div>
+      `
+}, function(tweets, tweetsDisplayed) {
+        $('.slider-twitter').unslider({
+          });
+    });
